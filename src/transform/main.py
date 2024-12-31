c1137925ea1490539e5187c5f06b0c49a10cee45
Andrews Goes <andrews.goes.33@gmail.com>
2024-12-31 19:06:03 -0300

    feat: dashboard streamlit finalizado

--- src/transform/main.py
+++ src/transform/main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import sqlite3
+from datetime import datetime
 
 df = pd.read_json('../data/data.jsonl', lines=True)
 
@@ -19,6 +20,8 @@ df["Reviews_Total"] = df["Reviews_Total"].fillna(0).astype(int)
 df['Old_Price_Total'] = df["Old_Price_Reais"] + df["Old_Price_Cents"] / 100
 df['New_Price_Total'] = df["New_Price_Reais"] + df["New_Price_Cents"] / 100
 
+df['Extraction_Date'] = pd.to_datetime(df['Extraction_Date'], errors='coerce').dt.strftime('%d-%m-%Y')
+
 df.drop(columns=['Old_Price_Reais', 'Old_Price_Cents', "New_Price_Reais", "New_Price_Cents"])
 
 nova_ordem = ["Marketplace", "Brand", "Name", 
@@ -33,4 +36,5 @@ conn = sqlite3.connect('../data/mercadolivre.db')
 df.to_sql('mercadolivre_items', conn, index=False, if_exists="replace")
 
 conn.close()
+
 
